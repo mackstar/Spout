d746c654a25aad6536537e4bc3354bbbe9d0b41e
Richard McIntyre <richard.mackstar@gmail.com>
2013-12-03 18:05:35 +0000

    Test added and working for admin directive.

--- apps/Admin/tests/js/tests/admin/RolesDirectiveSpec.js
+++ apps/Admin/tests/js/tests/admin/RolesDirectiveSpec.js
@@ -1,26 +1,30 @@
 describe('Roles Directive', function() {
 
     var scope,
-        element,
-        $element;
+        $element,
+        httpBackend;
 
-    beforeEach(angular.mock.module('Application'));
     beforeEach(function () {
-
+        angular.mock.module('Application');
         inject(
             function ($rootScope, $compile, _$httpBackend_) {
-                var element = angular.element('<div roles-selector></div>');
-                scope = $rootScope;
+                var element = angular.element('<roles-selector />'),
+                    httpBackend = _$httpBackend_;
+
+                httpBackend.whenGET('/users/roles').respond(
+                    '{"roles":[{"id":1, "name": "Admin"},{"id":2, "name": "Contributor"}]}'
+                );
                 scope = $rootScope;
                 $compile(element)(scope);
                 scope.$digest();
                 $element = $(element);
-
+                httpBackend.flush();
             }
         );
     });
 
     it('should have a select menu', function() {
         expect($element.prop("tagName")).toEqual('SELECT');
+        expect($element.find("option").length).toBe(2);
     });
 });
--- apps/Admin/var/www/js/directives/rolesDirectives.js
+++ apps/Admin/var/www/js/directives/rolesDirectives.js
@@ -3,13 +3,15 @@ app.directive(
     ['Restangular',
         function (Restangular) {
             return {
-                restrict: 'AC',
+                restrict: 'E',
                 transclude: true,
                 replace: true,
-                template: "<select><options></options></select>",
-                link: function (element, attributes) {
-                    
-
+                template: "<select ng-model='role' ng-options='role.name for role in roles'></select>",
+                link: function ($scope) {
+                    Restangular.all('users/roles').getList().then(function (data) {
+                        $scope.roles = data.roles;
+                        $scope.role = $scope.roles[0];
+                    });                    
                 }
             };
         }
