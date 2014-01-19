3a670315ff378b584f6614328b8e6a9e98bffa65
Akihito Koriyama <akihito.koriyama@gmail.com>
2014-01-19 11:01:01 +0900

    Update dev.php
    dev.php has more self contained procedure, remove develop/instance.php.

--- apps/Admin/bootstrap/contexts/dev.php
+++ apps/Admin/bootstrap/contexts/dev.php
@@ -1,4 +1,7 @@
 <?php
+
+use BEAR\Package\Dev\Dev;
+
 /**
  * CLI / Built-in web server script for development
  *
@@ -8,39 +11,47 @@
  *
  * CLI:
  * $ php web.php get /
- * $ php web.php get / prod
- * $ php web.php get / api
  *
  * Built-in web server:
  * $ php -S localhost:8080 web.php
  *
- * @global  $context string
+ * @global $context string
  */
 
 ob_start();
 
-$dir = dirname(dirname(__DIR__));
+if ((PHP_SAPI !== 'cli') && preg_match('/\.(?:png|jpg|jpeg|gif|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;
+}
 
-//
-// Here we get an application instance by setting a $context variable such as (Prod, Dev, Api, Stub, Test)
+$appDir = dirname(dirname(__DIR__));
+
+// Here we get an application instance by setting a $context variable such as (prod, dev, api)
 // the dev instance provides debugging tools and defaults to help you the development of your application.
-//
 $context = 'dev';
-$app = require $dir . '/bootstrap/develop/instance.php';
+$app = require $appDir . '/bootstrap/instance.php';
 /* @var $app \BEAR\Package\Provide\Application\AbstractApp */
+$dev = new Dev;
+$dev
+    ->iniSet()
+    ->loadDevFunctions()
+    ->registerFatalErrorHandler()
+    ->registerExceptionHandler("{$appDir}/var/log")
+    ->registerSyntaxErrorEdit()
+    ->setApp($app, $appDir)
+    ->serviceDevWeb();
+
+// When using the built in file-server when directly accessing files the app instance will not be created and
+// and the script will be exited.
+if ($dev->isDirectStaticFileAccess()) {
+    return false;
+}
 
 //
 // The cache is cleared on each request via the following script. We understand that you may want to debug
 // your application with caching turned on. When doing so just comment out the following.
 //
-//require $dir . '/bin/clear.php';
-
-//
-// When using the built in file-server when directly accessing files the app instance will not be created and
-// and the script will be exited.
-if (!$app) {
-    return false;
-}
+require $appDir . '/bin/clear.php';
 
 //
 // Calling the match of a BEAR.Sunday compatible router will give us the $method, $pagePath, $query to be used
