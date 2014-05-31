d2c3dc42b2d6c490a5fb23994436e76ab485f1a1
Richard McIntyre <richard.mackstar@gmail.com>
2014-05-31 19:34:42 +0100

    Able to select media when editing.

--- apps/Admin/var/www/js/apps/resources/routes.js
+++ apps/Admin/var/www/js/apps/resources/routes.js
@@ -18,27 +18,27 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
       }]
     }
   })
-  .state('resources.add', {
-    url: "/add/:type",
-    authenticate: true,
-    controller: 'ModalCtrl',
-    template: "<div ui-view></div>",
-    resolve: {
-      params: function () {
-        return {
-          templateUrl: "/js/templates/resources/add.html",
-          controller: 'ResourceAddCtrl',
-          onComplete: '^',
-          resolve: {
-            type: ['Restangular', '$stateParams', function (Restangular, $stateParams) {
-              return Restangular.one("resources/types").get({slug: $stateParams.type});
-            }]
-          }
-        };
+    .state('resources.add', {
+      url: "/add/:type",
+      authenticate: true,
+      controller: 'ModalCtrl',
+      template: "<div ui-view></div>",
+      resolve: {
+        params: function () {
+          return {
+            templateUrl: "/js/templates/resources/add.html",
+            controller: 'ResourceAddCtrl',
+            onComplete: '^',
+            resolve: {
+              type: ['Restangular', '$stateParams', function (Restangular, $stateParams) {
+                return Restangular.one("resources/types").get({slug: $stateParams.type});
+              }]
+            }
+          };
+        }
       }
-    }
-  })
-  .state('resources.add.media', {
+    })
+    .state('resources.add.media', {
       url: "/media/:field",
       authenticate: true,
       controller: 'ModalCtrl',
@@ -62,26 +62,51 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
         }
       }
     })
-  .state('resources.edit', {
-    url: "/edit/:type/:id",
-    authenticate: true,
-    controller: 'ModalCtrl',
-    resolve: {
-      params: function () {
-        return {
-          templateUrl: "/js/templates/resources/add.html",
-          controller: 'ResourceEditCtrl',
-          onComplete: '^',
-          resolve: {
-            resource: ['Restangular', '$stateParams', function (Restangular, $stateParams) {
-              return Restangular.one("resources/detail").get({id:$stateParams.id});
-            }],
-            type: ['Restangular', '$stateParams', function (Restangular, $stateParams) {
-              return Restangular.one("resources/types").get({slug: $stateParams.type});
-            }]
-          }
-        };
+    .state('resources.edit', {
+      url: "/edit/:type/:id",
+      authenticate: true,
+      template: "<div ui-view></div>",
+      controller: 'ModalCtrl',
+      resolve: {
+        params: function () {
+          return {
+            templateUrl: "/js/templates/resources/add.html",
+            controller: 'ResourceEditCtrl',
+            onComplete: '^',
+            resolve: {
+              resource: ['Restangular', '$stateParams', function (Restangular, $stateParams) {
+                return Restangular.one("resources/detail").get({id:$stateParams.id});
+              }],
+              type: ['Restangular', '$stateParams', function (Restangular, $stateParams) {
+                return Restangular.one("resources/types").get({slug: $stateParams.type});
+              }]
+            }
+          };
+        }
       }
-    }
-  });
+    })
+    .state('resources.edit.media', {
+      url: "/media/:field",
+      authenticate: true,
+      controller: 'ModalCtrl',
+      resolve: {
+        params: function () {
+          return {
+            windowClass: "sp-modal__double",
+            templateUrl: "/js/templates/resources/media.html",
+            controller: 'ResourceMediaAddCtrl',
+            onComplete: '^',
+            reload: false,
+            resolve: {
+              media: ['Restangular', function (Restangular) {
+                return Restangular.all('media/index').getList()
+              }],
+              field: ['$stateParams', function ($stateParams) {
+                return $stateParams.field;
+              }]
+            }
+          };
+        }
+      }
+    });
 }]);
