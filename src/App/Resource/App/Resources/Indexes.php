4b750f596067db0a50b2f405174d913499e13ca5
Richard McIntyre <richard.mackstar@gmail.com>
2014-07-24 12:06:05 +0900

    Fixing caching of views in each request.

---
+++ src/App/Resource/App/Resources/Indexes.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * This file is part of the Mackstar.Spout package.
+ *
+ * (c) Richard McIntyre <richard.mackstar@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mackstar\Spout\App\Resource\App\Resources;
+
+use BEAR\Resource\ResourceObject;
+use BEAR\Package\Module\Database\Dbal\Setter\DbSetterTrait;
+use BEAR\Sunday\Annotation\Db;
+
+/**
+ * FieldTypes
+ *
+ * @Db
+ */
+class Indexes extends ResourceObject
+{
+    use DbSetterTrait;
+
+    protected $table = 'resource_indexes';
+
+    public function onGet($slug = null)
+    {
+
+        $sql  = "SELECT {$this->table}.* FROM {$this->table} ";
+        $this['indexes'] = $this->db->fetchAll($sql);
+
+        return $this;
+    }
+
+    public function onPost($slug = null)
+    {
+
+        $sql  = "SELECT {$this->table}.* FROM {$this->table} ";
+        $this['indexes'] = $this->db->fetchAll($sql);
+
+        return $this;
+    }
+
+    public function paginated() {
+
+    }
+
+    public function nonPaginated() {
+        
+    }
+
+}
